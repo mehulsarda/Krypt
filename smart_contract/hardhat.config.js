457c10ab58e3a71155cc90fe677c59199a2e12f9
Mehul <mehulsarda2002@gmail.com>
2022-07-11 19:52:55 +0530

    Solidity and Deploy file completed

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,14 @@
+// https://eth-goerli.g.alchemy.com/v2/WbnJ2bT9a8LQwly9Is2VElH5pamEC8Vu
+
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.9",
+// };
+
+require('@nomiclabs/hardhat-waffle')
+
+module.exports = {
+
+}
---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,37 @@
+
+const hre = require("hardhat");
+
+const main = async () => {
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
+
+  // const Lock = await hre.ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  // await lock.deployed();
+
+  // console.log("Lock with 1 ETH deployed to:", lock.address);
+
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
+
+  await transactions.deployed();
+
+  console.log("Transactions deployed to:", transactions.address);
+
+}
+
+const runmain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runmain();
